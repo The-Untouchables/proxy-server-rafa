20351c5240f37e1e5a56c894bed879c48647c768
Rafael Hernandez <rhmnospam@gmail.com>
2018-03-09 12:05:07 -0800

    Add local bookings component. Add deployed neighborhood component

--- server/api-router.js
+++ server/api-router.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+let descHost = process.env.DESCRIPTION_SERVICE_HOST || 'localhost';
+let descPort = process.env.DESCRIPTION_SERVICE_PORT || '3000';
+
+let photoHost = process.env.CAROUSEL_SERVICE_HOST || 'localhost';
+let photoPort = process.env.CAROUSEL_SERVICE_PORT || '3001';
+
+let reviewsHost = process.env.REVIEWS_SERVICE_HOST || 'localhost';
+let reviewsPort = process.env.CAROUSEL_SERVICE_PORT || '3004';
+
+let neighborhoodHost = process.env.NEIGHBORHOOD_SERVICE_HOST || 'localhost';
+let neighborhoodPort = process.env.NEIGHBORHOOD_SERVICE_PORT || '3006';
+
 router.route('/:roomid/description')
-  .get((req, res, next) => res.redirect(`http://localhost:3002/api/rooms/${req.params.roomid}/description`));
+  .get((req, res, next) => res.redirect(`http://${descHost}:${descPort}/api/rooms/${req.params.roomid}/description`));
+
+router.route('/:roomid/carousel')
+  .get((req, res, next) => res.redirect(`http://${photoHost}:${photoPort}/api/rooms/${req.params.roomid}/carousel`));
+
+router.route('/:roomid/neighborhood')
+  .get((req, res, next) => res.redirect(`http://${neighborhoodHost}:${neighborhoodPort}/api/rooms/${req.params.roomid}/neighborhood`));
 
 module.exports = router;
--- server/rooms-router.js
+++ server/rooms-router.js
@@ -2,6 +2,18 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+let descHost = process.env.DESCRIPTION_SERVICE_HOST || 'localhost';
+let descPort = process.env.DESCRIPTION_SERVICE_PORT || '3000';
+
+let photoHost = process.env.CAROUSEL_SERVICE_HOST || 'localhost';
+let photoPort = process.env.CAROUSEL_SERVICE_PORT || '3001';
+
+let reviewsHost = process.env.REVIEWS_SERVICE_HOST || 'localhost';
+let reviewsPort = process.env.REVIEWS_SERVICE_PORT || '3004';
+
+let neighborhoodHost = process.env.NEIGHBORHOOD_SERVICE_HOST || 'localhost';
+let neighborhoodPort = process.env.NEIGHBORHOOD_SERVICE_PORT || '3006';
+
 router.route('/:roomid')
   .get((req, res, next) => res.sendFile('index.html', {root: path.resolve('public')}))
   .options((req, res) => {
@@ -9,12 +21,21 @@ router.route('/:roomid')
   });
 
 router.route('/:roomid/reviews')
-  .get((req, res, next) => res.redirect(`http://localhost:3004/rooms/${req.params.roomid}/reviews`));
+  .get((req, res, next) => res.redirect(`http://${reviewsHost}:${reviewsPort}/rooms/${req.params.roomid}/reviews`));
 
 router.route('/:roomid/ratings')
-  .get((req, res, next) => res.redirect(`http://localhost:3004/rooms/${req.params.roomid}/ratings`));
+  .get((req, res, next) => res.redirect(`http://${reviewsHost}:${reviewsPort}/rooms/${req.params.roomid}/ratings`));
 
 router.route('/:roomid/description')
-  .get((req, res, next) => res.redirect(`http://localhost:3002/rooms/${req.params.roomid}/description`));
+  .get((req, res, next) => res.redirect(`http://${descHost}:${descPort}/rooms/${req.params.roomid}/description`));
+
+router.route('/:roomid/carousel')
+  .get((req, res, next) => res.redirect(`http://${photoHost}:${photoPort}/rooms/${req.params.roomid}/carousel`));
+
+router.route('/:roomid/bookings')
+  .get((req, res, next) => res.redirect(`http://localhost:3003/rooms/${req.params.roomid}/bookings`));
+
+router.route('/:roomid/neighborhood')
+  .get((req, res, next) => res.redirect(`http:/${neighborhoodHost}:${neighborhoodPort}/rooms/${req.params.roomid}/neighborhood`));
 
 module.exports = router;
